ae0a09071bef1f8d9cea3967224a056d02c1c65e
Stephen Jennings <stephen.g.jennings@gmail.com>
2016-10-12 19:01:06 -0700

    Add query for a running process

--- sqlserver/currently-running-queries.sql
+++ sqlserver/currently-running-queries.sql
@@ -1,4 +1,5 @@
--- From: http://blog.sqlauthority.com/2009/01/07/sql-server-find-currently-running-query-t-sql/
+-- To view currently-executing processes and the query they're executing:
+-- (from: http://blog.sqlauthority.com/2009/01/07/sql-server-find-currently-running-query-t-sql/)
 
 SELECT
     sqltext.TEXT,
@@ -14,3 +15,22 @@ CROSS APPLY sys.dm_exec_sql_text(sql_handle) AS sqltext
 -- To kill a running process by session_id:
 
 KILL [session_id]
+
+
+-- To view information about a process:
+-- (from: http://blog.sqlauthority.com/2015/08/31/sql-server-spid-is-killedrollback-state-what-to-do-next/)
+
+SELECT
+    spid
+    ,kpid
+    ,login_time
+    ,last_batch
+    ,status
+    ,hostname
+    ,nt_username
+    ,loginame
+    ,hostprocess
+    ,cpu
+    ,memusage
+    ,physical_io
+FROM sys.sysprocesses
